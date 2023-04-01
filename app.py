bb30a94a4316896e47500d74b6d40046326c1392
adnan4290 <shahzadadnan429@gmail.com>
2023-04-01 22:16:59 +0500

    Exception-Handling done

--- app.py
+++ app.py
@@ -1,47 +1,43 @@
 from flask import Flask, render_template, Response
 from camera_stream import gen_frames
-from flask_socketio import SocketIO, emit
+from input_handler import handle_input
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-app.debug = True
+app.debug=True
 socketio = SocketIO(app)
 
 latitude = None
 longitude = None
 
-
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
 @app.route('/control', methods=['POST'])
 def control():
-    # handle_input function not defined in your code
-    message = "control request received"
+    message = handle_input()
     print(message)
     return message
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/switch_camera', methods=['POST', 'GET'])
+@app.route('/switch_camera', methods=['POST','GET'])
 def switchcamera():
     print("switch camera route accessed")
     # Call the function to switch cameras
     # switch_camera()
     return "switch camera route finished running"
 
-
+# code for displaying latitude and longitude 
 @app.route('/latitude')
 def get_latitude():
     global latitude
     return str(latitude)
 
-
+# endpoint to get the longitude value
 @app.route('/longitude')
 def get_longitude():
     global longitude
@@ -58,12 +54,15 @@ def handle_disconnect():
     print('Client disconnected')
 
 
-def send_frame():
-    while True:
-        for frame in gen_frames():
-            socketio.emit('frame', frame, broadcast=True)
+def send_frame(frame):
+    emit('frame', frame)
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
 
 if __name__ == '__main__':
-    socketio.start_background_task(target=send_frame)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    try:
+        socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    finally:
+        socketio.stop()
